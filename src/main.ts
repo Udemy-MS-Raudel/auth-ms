8d82c1b4eb43dc6f3ecad4f5aad0384227fac69c
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2025-01-06 12:25:01 +0100

    conectar el ms al gateway y config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  const logger = new Logger('MAIN AUTH')
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers
+    }
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+   );
+   
+  await app.listen();
+  logger.log(`App running on port ${envs.port}`);
 }
 bootstrap();
